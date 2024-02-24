1c3d58a1aacc43791921ac110e5b820d60bd7d33
PaulHarangusAndrei <147636835+PaulHarangusAndrei@users.noreply.github.com>
2024-02-25 01:11:29 +0200

    auth

--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -1,18 +1,23 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, useNavigate} from "react-router-dom";
 import {LogIn} from "../LogInPage/LogIn";
 import {Register} from "../RegisterPage/Register";
 import React from "react";
 import {HomePage} from "../HomePage/HomePage";
+import {ProtectedRoute} from "../../../utils/router/ProtectedRoute";
 
 
 
 export const MainPage = () => {
+
     return (
         <Router>
             <div className="App">
                 <div className="content">
                     <Routes>
-                        <Route path="/" element={<HomePage/>}/>
+                        <Route element={<ProtectedRoute/>}>
+                            <Route path="/" element={<HomePage/>}/>
+                        </Route>
+
                         <Route path="/login" element={<LogIn/>}/>
                         <Route path="/register" element={<Register/>}/>
                     </Routes>
