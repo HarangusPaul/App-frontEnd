78f47af86681d1d21143b95d6feab74bc9819704
Stefan Lupu <sebastefan42@gmail.com>
2024-03-04 16:04:56 +0200

    A modal for displaying basic information

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,12 +1,19 @@
 import "./HomePage.css"
 import {Button} from "semantic-ui-react";
+import React, {useState} from "react";
+import {ModalProps, ResponsiveModal} from "../../Modals/Dialog/ResponsiveModal/ResponsiveModal";
 
 export const HomePage = () => {
+    const[state,setState] = useState<boolean>(false)
 
     return (
         <div>
             <div className={"backGround"}>
                 <div className={"appView"}>
+                    {/*<Button onClick={() => setState(true)}>*/}
+                    {/*    Tiny*/}
+                    {/*</Button>*/}
+                    {/*<ResponsiveModal props={{open:state,setOpen:setState,text:"abc",header:"aaaa",messageButton:"nu am chef!"}}/>*/}
                 </div>
             </div>
         </div>
