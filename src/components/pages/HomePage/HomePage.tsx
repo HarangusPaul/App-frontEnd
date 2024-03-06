2046084a5dc925c7ebd90bffe7acc6ec31e7d21a
Stefan Lupu <sebastefan42@gmail.com>
2024-03-06 13:35:50 +0200

    A model to make you accept or not a thing. (2 buttons)

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,12 +1,20 @@
 import "./HomePage.css"
 import {Button} from "semantic-ui-react";
+import {ApprovalModal} from "../../Modals/Dialog/ApprovementModal/ApprovalModal";
+import {useState} from "react";
 
 export const HomePage = () => {
+
+    const[state,setState] = useState<boolean>(false)
 
     return (
         <div>
             <div className={"backGround"}>
                 <div className={"appView"}>
+                    <Button onClick={() => setState(true)}>
+                        ApprovementModal
+                    </Button>
+                    <ApprovalModal props={{open:state,setOpen:setState,text:"You've been logged in successfully!",header:"Login",messageButton:"Ok"}}/>
                 </div>
             </div>
         </div>
