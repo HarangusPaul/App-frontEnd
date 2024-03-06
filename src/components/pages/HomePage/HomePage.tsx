238f3d6de1ada7ae76f2a94a2df6df2c87c8737e
Stefan Lupu <sebastefan42@gmail.com>
2024-03-06 12:50:57 +0200

    Modal for error display. Should contain and display an error

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,5 +1,8 @@
 import "./HomePage.css"
 import {Button} from "semantic-ui-react";
+import {ErrorModal} from "../../Modals/Dialog/ErrorModal/ErrorModal";
+
+
 
 export const HomePage = () => {
 
@@ -7,6 +10,13 @@ export const HomePage = () => {
         <div>
             <div className={"backGround"}>
                 <div className={"appView"}>
+                    <Button onClick={() => setState(true)}>
+                        Sure, retry!
+                    </Button>
+                    <Button onClick={() => setState(true)}>
+                        Sure, exit!
+                    </Button>
+                    <ErrorModal props={{open:state,setOpen:setState,text:"You've been logged in successfully!",header:"Login",messageButton:"Ok"}}/>
                 </div>
             </div>
         </div>
