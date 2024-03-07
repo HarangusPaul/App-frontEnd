a01bf22fa4e0eabc90cd9021c989e05016589e27
PaulHarangusAndrei <147636835+PaulHarangusAndrei@users.noreply.github.com>
2024-03-07 12:41:06 +0200

    Method to get to register/login page from each other

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ import {useState} from "react";
 
 import {ModalProps, ResponsesModal} from "../../Modals/Dialog/ResponsesModal/ResponsesModal";
 import {ApprovalModal} from "../../Modals/Dialog/ApprovementModal/ApprovalModal";
-import DropdownButton from "../../Inputs/Buttons/DropdownButton/DropdownButton";
+import DropdownButton from "../../Inputs/buttons/DropdownButton/DropdownButton";
 
 export const HomePage = () => {
 
@@ -15,17 +15,17 @@ export const HomePage = () => {
         <div>
             <div className={"backGround"}>
                 <div className={"appView"}>
-                    <Button onClick={() => setState(true)}>
-                        ApprovementModal
-                    </Button>
-                    <ApprovalModal props={{open:state,setOpen:setState,text:"Do you want to do this knowing the consequences?",header:"Approve",messageButton1:"Approve", messageButton2: "Reject"}}/>
-                    <Button onClick={() => setState(true)}>
-                        Login
-                    </Button>
-                    <ResponsesModal props={{open:state,setOpen:setState,text:"You've been logged in successfully!",header:"Login",messageButton:"Ok"}}/>
-                    <Button onClick={() => setState(true)}>
-                        Sure, retry!
-                    </Button>
+                    {/*<Button onClick={() => setState(true)}>*/}
+                    {/*    ApprovementModal*/}
+                    {/*</Button>*/}
+                    {/*<ApprovalModal props={{open:state,setOpen:setState,text:"Do you want to do this knowing the consequences?",header:"Approve",messageButton1:"Approve", messageButton2: "Reject"}}/>*/}
+                    {/*<Button onClick={() => setState(true)}>*/}
+                    {/*    Login*/}
+                    {/*</Button>*/}
+                    {/*<ResponsesModal props={{open:state,setOpen:setState,text:"You've been logged in successfully!",header:"Login",messageButton:"Ok"}}/>*/}
+                    {/*<Button onClick={() => setState(true)}>*/}
+                    {/*    Sure, retry!*/}
+                    {/*</Button>*/}
                     {/*
                     multiple: attribute can be 'true' or 'false'
                     search: attribute can be 'true' or 'false'
