02f279ba6c166aded7ccf3d64f7c8e18e00e1f42
Stefan Lupu <sebastefan42@gmail.com>
2024-03-07 15:13:40 +0200

    DropdownButton field it will get an option or more from a list of options. Fully generic (at least I think it is)

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -9,29 +9,52 @@ import DropdownButton from "../../Inputs/Buttons/DropdownButton/DropdownButton";
 
 export const HomePage = () => {
 
+
+    const dynamicOptions = [
+        { key: 'angular', text: 'Angular', value: 'angular' },
+        { key: 'css', text: 'CSS', value: 'css' },
+        { key: 'design', text: 'Graphic Design', value: 'design' },
+        { key: 'ember', text: 'Ember', value: 'ember' },
+        { key: 'html', text: 'HTML', value: 'html' },
+        { key: 'ia', text: 'Information Architecture', value: 'ia' },
+        { key: 'javascript', text: 'Javascript', value: 'javascript' },
+        { key: 'mech', text: 'Mechanical Engineering', value: 'mech' },
+        { key: 'meteor', text: 'Meteor', value: 'meteor' },
+        { key: 'node', text: 'NodeJS', value: 'node' },
+        { key: 'plumbing', text: 'Plumbing', value: 'plumbing' },
+        { key: 'python', text: 'Python', value: 'python' },
+        { key: 'rails', text: 'Rails', value: 'rails' },
+        { key: 'react', text: 'React', value: 'react' },
+        { key: 'repair', text: 'Kitchen Repair', value: 'repair' },
+        { key: 'ruby', text: 'Ruby', value: 'ruby' },
+        { key: 'ui', text: 'UI Design', value: 'ui' },
+        { key: 'ux', text: 'User Experience', value: 'ux' },
+    ];
+
+
     const[state,setState] = useState<boolean>(false)
 
     return (
         <div>
             <div className={"backGround"}>
                 <div className={"appView"}>
-                    <Button onClick={() => setState(true)}>
-                        ApprovementModal
-                    </Button>
-                    <ApprovalModal props={{open:state,setOpen:setState,text:"Do you want to do this knowing the consequences?",header:"Approve",messageButton1:"Approve", messageButton2: "Reject"}}/>
-                    <Button onClick={() => setState(true)}>
-                        Login
-                    </Button>
-                    <ResponsesModal props={{open:state,setOpen:setState,text:"You've been logged in successfully!",header:"Login",messageButton:"Ok"}}/>
-                    <Button onClick={() => setState(true)}>
-                        Sure, retry!
-                    </Button>
+                    {/*<Button onClick={() => setState(true)}>*/}
+                    {/*    ApprovementModal*/}
+                    {/*</Button>*/}
+                    {/*<ApprovalModal props={{open:state,setOpen:setState,text:"Do you want to do this knowing the consequences?",header:"Approve",messageButton1:"Approve", messageButton2: "Reject"}}/>*/}
+                    {/*<Button onClick={() => setState(true)}>*/}
+                    {/*    Login*/}
+                    {/*</Button>*/}
+                    {/*<ResponsesModal props={{open:state,setOpen:setState,text:"You've been logged in successfully!",header:"Login",messageButton:"Ok"}}/>*/}
+                    {/*<Button onClick={() => setState(true)}>*/}
+                    {/*    Sure, retry!*/}
+                    {/*</Button>*/}
                     {/*
                     multiple: attribute can be 'true' or 'false'
                     search: attribute can be 'true' or 'false'
                     */
                     }
-                    <DropdownButton multiple={true} search={true}/>
+                    {/*<DropdownButton multiple={true} search={true} options={dynamicOptions}/>*/}
                 </div>
                 <ErrorModal props={{open:state,setOpen:setState,text:"You've encountered an error!",header:"Error",messageButton1:"Sure, exit!", messageButton2:"Sure, Retry!"}}/>
             </div>
