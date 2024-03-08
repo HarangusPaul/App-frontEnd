71386a8d60ca0359d1a7f6fe01ac9d85ab1cf909
Stefan Lupu <sebastefan42@gmail.com>
2024-03-08 17:05:49 +0200

    A modal with a list of pickable items. It should include the option picker component

--- src/services/api/UserAxis.tsx
+++ src/services/api/UserAxis.tsx
@@ -5,7 +5,6 @@ import {TokenModel} from "../models/TokenModel";
 import {UserLoginModel} from "../Models/UserLoginModel";
 
 
-
 const ipServer = "http://localhost:8080/app/v1";
 
 export class UserService {
@@ -14,11 +13,40 @@ export class UserService {
         return axios
             .post(
                 `${ipServer}/accounts/login`,
-                    userReq,
-
+                userReq,
             )
-                    .then(function (response:AxiosResponse<TokenModel>) {
+            .then(function (response: AxiosResponse<TokenModel>) {
                 return response.data;
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    getToken(token1: string) {
+        const config = {
+            headers: {Authorization: `Bearer ${token1}`}
+        };
+        // axios.defaults.headers.common = {
+        //     "Content-Type": "application/json",
+        //     "Paul": "Paul",
+        //     "Access-Control-Allow-Origin": "*",
+        //     "Access-Control-Allow-Credentials":"true",
+        //     'Authorization': 'Bearer ' + token1
+        // };
+        return axios
+            .get(
+                `${ipServer}/accounts/valid-token`,
+                {
+                    headers: {
+                        "Authorization": `Bearer ${token1}`, "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+                    }
+                }
+            )
+            .then(function (response: AxiosResponse<any>) {
+                return response.status === 200;
 
             })
             .catch(function (error) {
--- src/utils/router/CredentialVerifier.tsx
+++ src/utils/router/CredentialVerifier.tsx
@@ -1,14 +1,24 @@
+import {UserService} from "../../services/api/UserAxis";
+import {useState} from "react";
+
 export const credentialVerifier = () => {
+    const userService:UserService = new UserService();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [token1, setToken] = useState<boolean>(false);
+    // let token = localStorage.getItem("token");
 
     function tokenIsValid(token: string) {
         //todo:token verifier
-        return token == "";
+        const obj = JSON.parse(token)
+        userService.getToken(obj.token).then((response) => {
+            setToken(response?response:false) ;
+        });
     }
 
     let allReadyLoggedOn = () => {
         let token = localStorage.getItem("token");
-
-        return token == null || tokenIsValid(token);
+        tokenIsValid(token?token:"");
+        return token == null || token1;
     }
 
     return allReadyLoggedOn();
