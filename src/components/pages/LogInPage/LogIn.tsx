25409f08e6aeac2a3ec095d8f60486ab32e475ac
PaulHarangusAndrei <147636835+PaulHarangusAndrei@users.noreply.github.com>
2024-03-09 12:39:57 +0200

    Method to get to register/login page from each other

--- src/components/pages/LogInPage/LogIn.tsx
+++ src/components/pages/LogInPage/LogIn.tsx
@@ -7,7 +7,7 @@ export const LogIn = () => {
 
     return (
         <div className={"backGround"}>
-            <div className={"LogInContainer"}>
+            <div className={"LogInContainerLogIn"}>
                 <Image src='https://react.semantic-ui.com/images/wireframe/image.png' className={"Logo"} size='small' wrapped />
                 <LogInForm></LogInForm>
             </div>
